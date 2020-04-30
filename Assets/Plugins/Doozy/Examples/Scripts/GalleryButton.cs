077839e3a3ec16e20e9e5b0af2a845e0caf5676d
Aurélien Thazet <thazet.aurelien@gmail.com>
2020-04-30 14:56:48 +0200

    first commit

---
+++ Assets/Plugins/Doozy/Examples/Scripts/GalleryButton.cs
@@ -0,0 +1,29 @@
+﻿using Doozy.Engine.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Doozy.Examples
+{
+    public class GalleryButton : MonoBehaviour
+    {
+        public GameObject prefabPopup;
+        private UIPopup popup;
+
+        public void Show()
+        {
+            popup = UIPopup.GetPopup(prefabPopup.name);
+
+            if (popup == null)
+                return;
+
+            popup.HideOnBackButton = false;
+            popup.HideOnClickAnywhere = true;
+            popup.HideOnClickOverlay = true;
+            popup.HideOnClickContainer = true;
+
+            popup.AutoHideAfterShow = false;
+
+            popup.Show();
+        }
+    }
+}
---
+++ Assets/Plugins/Doozy/Examples/Scripts/Popup.cs
@@ -0,0 +1,20 @@
+﻿using Doozy.Engine.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Doozy.Examples
+{
+    public class Popup : MonoBehaviour
+    {
+        public Sprite newImage;
+        public GameObject imageGameObject;
+
+        void Start()
+        {
+            imageGameObject = gameObject.transform.GetChild(1).GetChild(0).gameObject;
+            imageGameObject.GetComponent<Image>().sprite = newImage;
+
+            //GameObject.Find("ImageInPopup").GetComponent<Image>().sprite = newImage;
+        }
+    }
+}
